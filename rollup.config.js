421380dc9d63d68218461a26d8b95a3171cd5026
Steve Rubin <nerdforlife@gmail.com>
2021-08-17 15:32:27 -0700

    Package with rollup

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import pkg from './package.json';
+import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
+
+export default {
+  input: 'index.js',
+  plugins: [
+    commonjs(),
+    copy({
+      targets: [
+        { src: 'index.d.ts', dest: 'dist'},
+        { src: 'superpowered/**/*.d.ts', dest: 'dist/superpowered'},
+      ],
+    }),
+  ],
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+    },
+  ],
+};
