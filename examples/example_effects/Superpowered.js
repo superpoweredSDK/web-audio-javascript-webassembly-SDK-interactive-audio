b222f8c57800d2800fe82aee8974bc2a5bdb7da7
Thomas Dodds <>
2024-05-24 15:15:49 +0100

    Superpowered JS SDK v2.6.8 1) Fix for AudioInMemory dynamic content loading 2) Fix for potential infinite loop in AAC decoding

--- examples/example_effects/Superpowered.js
+++ examples/example_effects/Superpowered.js
@@ -3,7 +3,7 @@
 
 class SuperpoweredGlue {
 
-    static wasmCDNUrl = "https://cdn.jsdelivr.net/npm/@superpoweredsdk/web@2.6.7/dist/superpowered-npm.wasm"
+    static wasmCDNUrl = "https://cdn.jsdelivr.net/npm/@superpoweredsdk/web@2.6.8/dist/superpowered-npm.wasm"
 
     niceSize(bytes) {
         if (bytes == 0) return '0 byte'; else if (bytes == 1) return '1 byte';
